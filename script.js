c9d83e51526bbeadac1fc918088b00f5ad675e9e
Wlauber <wlaubersamuel@gmail.com>
2022-10-11 22:21:20 -0300

    Adiciona quadro com 25 pixels e modifica tamanho do pixel para 40px

--- script.js
+++ script.js
@@ -9,11 +9,11 @@ function divMae() {
         const divMae = document.createElement('div');
         divMae.type = 'color';
         divMae.style.backgroundColor = 'white';
-        divMae.style.border = 'solid 1px black';
-        divMae.style.borderRadius = '50px';
-        divMae.style.width = '50px';
-        divMae.style.height = '50px';
-        const color = document.getElementById('color-palette')
+        divMae.style.width = '40px';
+        divMae.style.height = '40px';
+        divMae.style.border = '1px solid black';
+        divMae.style.display = 'inline-block';
+        const color = document.getElementById('color-palette');
         divMae.className = 'color';
         color.appendChild(divMae);
     }
@@ -21,19 +21,17 @@ function divMae() {
 }
 
 function coresAleatorias() {
-        const r = Math.floor(Math.random() * 255);
-        const g = Math.floor(Math.random() * 255);
-        const b = Math.floor(Math.random() * 255);
-        return `rgb(${r},${g},${b})`;
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    return `rgb(${r},${g},${b})`;
 }
-    
+
 function paint() {
     const cor = document.querySelectorAll('.color');
     cor[0].style.backgroundColor = 'black';
     for (let index = 1; index < cor.length; index += 1) {
         cor[index].style.backgroundColor = coresAleatorias();
-
-        
     }
 }
 
@@ -41,9 +39,24 @@ function aleatorioBtn() {
     const btn = document.getElementById('button-random-color');
     btn.addEventListener('click', paint);
 }
+
+function pixel() {
+    for (let index = 0; index < 25; index += 1) {
+        const divMae = document.createElement('div');
+        // divMae.type = 'pixel';
+        divMae.style.backgroundColor = 'white';
+        divMae.style.border = '1px solid black';
+        divMae.style.width = '40px';
+        divMae.style.height = '40px';
+        const color = document.getElementById('pixel-board');
+        divMae.className = 'pixel';
+        color.appendChild(divMae);
+    }
+}
 divMae();
 aleatorioBtn();
 paint();
+pixel();
 
 // function createButton() {
 //   const getSection = document.querySelector('#color-palette')[0];
@@ -75,4 +88,3 @@ paint();
 // };
 // createButton();
 // createColors();
-// colorRandom();
